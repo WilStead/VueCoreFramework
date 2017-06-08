d2d10a2751cba17fe1c0b500ef45c024247253d9
Wil Stead <wil.stead@gmail.com>
2017-06-07 20:28:12 -0400

    Auto-Vuetify timespan

--- ClientApp/dynamic-data/dynamic-form/fieldVuetifyDateTime.ts
+++ ClientApp/dynamic-data/dynamic-form/fieldVuetifyDateTime.ts
@@ -1,4 +1,5 @@
 ﻿import { abstractField } from 'vue-form-generator';
+import * as moment from 'moment';
 
 export default {
     mixins: [abstractField],
@@ -7,34 +8,18 @@ export default {
             formattedDate: new Date(this.value).toDateString(),
             menuDate: false,
             menuTime: false,
-            valueTime: this.getShortTime(this.value)
+            valueTime: moment(this.value).format("h:mma")
         }
     },
     methods: {
         formatValueToModel(value) {
             return value;
-        },
-        getShortTime(value) {
-            let d = new Date(value);
-            let h = d.getHours();
-            let am = h < 12;
-            if (!am && h !== 12) {
-                h -= 12;
-            } else if (am && h === 0) {
-                h += 12;
-            }
-            let m = d.getMinutes();
-            return h + ":" + (m < 10 ? "0" : "") + m + (am ? "am" : "pm");
         }
     },
     watch: {
         valueTime(newValue) {
-            let [h, m] = newValue.split(':');
-            let a = m.substring(2);
-            m = m.substring(0, 2);
-            let val = new Date(this.value);
-            val.setUTCHours(a === 'am' ? parseInt(h) : parseInt(h) + 12, parseInt(m));
-            this.value = val.toISOString();
+            let t = moment(newValue, "h:mma");
+            this.value = moment(this.value).utc().set({ 'hour': t.hours(), 'minute': t.minutes() }).toISOString();
         }
     }
 };
--- ClientApp/dynamic-data/dynamic-form/fieldVuetifyTimespan.ts
+++ ClientApp/dynamic-data/dynamic-form/fieldVuetifyTimespan.ts
@@ -1,9 +1,11 @@
 ﻿import { abstractField } from 'vue-form-generator';
+import * as moment from 'moment';
 
 export default {
     mixins: [abstractField],
     data() {
         return {
+            duration: moment.duration(this.value),
             dayValue: 0,
             hourValue: 0,
             minuteValue: 0,
@@ -18,18 +20,28 @@ export default {
             return value;
         },
         onDayChange(newValue) {
+            this.duration.subtract(this.duration.days(), 'd');
+            this.duration.add(newValue, 'd');
             this.updateValue();
         },
         onHourChange(newValue) {
+            this.duration.subtract(this.duration.hours(), 'h');
+            this.duration.add(newValue, 'h');
             this.updateValue();
         },
         onMinuteChange(newValue) {
+            this.duration.subtract(this.duration.minutes(), 'm');
+            this.duration.add(newValue, 'm');
             this.updateValue();
         },
         onSecondChange(newValue) {
+            this.duration.subtract(this.duration.milliseconds(), 'ms');
+            this.duration.subtract(this.duration.seconds(), 's');
+            this.duration.add(newValue, 's');
             this.updateValue();
         },
         updateValue() {
+            this.value = moment.utc(this.duration.asMilliseconds()).format("D.HH:mm:ss.SSS");
         }
     }
 };
