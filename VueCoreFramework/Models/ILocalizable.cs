5928f81482a2f2ef8859f6c8f0661e561dbd2f96
Wil Stead <wil.stead@gmail.com>
2017-07-15 17:46:31 -0400

    Documentation

--- VueCoreFramework/Models/ILocalizable.cs
+++ VueCoreFramework/Models/ILocalizable.cs
@@ -2,6 +2,11 @@
 
 namespace VueCoreFramework.Models
 {
+    /// <summary>
+    /// If your data classes implement this interface, their ViewModels will automatically invoke
+    /// this variation of <see cref="ToString"/> with the current <see cref="IStringLocalizer"/> so
+    /// that you can properly localize the result.
+    /// </summary>
     interface ILocalizable
     {
         string ToString(IStringLocalizer localizer);
